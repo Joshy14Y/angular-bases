2537f336f2a380fed2bc1205c4d4608d6bb52d69
Joshy14Y <joshygamer14y@gmail.com>
2024-11-28 23:51:30 -0600

    Fin Sección 5

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,12 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from './components/heroes/hero/hero.component';
 import { ListComponent } from './components/heroes/list/list.component';
 
-
 @NgModule({
   declarations: [HeroComponent, ListComponent],
   exports: [HeroComponent, ListComponent],
-  imports:[
-    CommonModule
-  ]
+  imports: [CommonModule],
 })
 export class HeroesModule {}
